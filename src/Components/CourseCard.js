451dd42924bfd3bc5696a26ce7c0cfa3cb6c39ef
Hinakouser <Hinakouser118@gmail.com>
2023-05-19 11:36:16 +0530

    Cards

--- src/Components/CourseCard.js
+++ src/Components/CourseCard.js
@@ -4,9 +4,43 @@ export default function CourseCard() {
   return (
     <div>
       <br></br>
-      <h1 style={{textAlign:'center'}}>Razorpay</h1>
+      <h1 style={{textAlign:'center'}}>Louis Vuitton</h1>
       <br></br>
-      <button type="button" onClick={displayRazorpay} className='course-payment-button'> Buy Course</button>
+      <div className="container text-center">
+  <div className="row">
+    <div className="col-md-4">
+    <div className="card" style={{width: "18rem"}}>
+  <img src="https://tse1.mm.bing.net/th?id=OIP.JbBQYCEhxzxzkG_qMeQ9WwHaHa&pid=Api&P=0&h=180" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">HandBag</h5>
+    <h5 className="card-title">₹5000</h5>
+    <button type="button" onClick={()=>displayRazorpay({amt:500000,currency:"INR",name:"HandBag"})} className='course-payment-button'> Buy Course</button>
+  </div>
+</div>
+    </div>
+    <div className="col-md-4">
+    <div className="card" style={{width: "18rem"}}>
+  <img src="https://tse3.mm.bing.net/th?id=OIP.vjIgy89sUXhnxGyIFJjFHQAAAA&pid=Api&P=0&h=180" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Watch</h5>
+    <h5 className="card-title">₹2500</h5>
+    <button type="button" onClick={()=>displayRazorpay({amt:250000,currency:"INR",name:"Watch"})} className='course-payment-button'> Buy Course</button>
+  </div>
+</div>
+    </div>
+    <div className="col-md-4">
+    <div className="card" style={{width: "18rem"}}>
+  <img src="https://tse4.mm.bing.net/th?id=OIP.4kiVNGkEUetEDZ7xXyIaTQHaGl&pid=Api&P=0&h=180" className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">Slipers</h5>
+    <h5 className='card-title'>₹2000</h5>
+    <button type="button" onClick={()=>displayRazorpay({amt:20000,currency:"INR",name:"Slipers"})} className='course-payment-button'> Buy Course</button>
+   </div>
+</div>
+    </div>
+  </div>
+</div>
+      {/* <button type="button" onClick={displayRazorpay} className='course-payment-button'> Buy Course</button> */}
     </div>
   )
 }
--- src/utils/paymentGateway.js
+++ src/utils/paymentGateway.js
@@ -1,24 +1,31 @@
-export default async function displayRazorpay(){
+export default async function displayRazorpay(props){
 // const data = await fetch("http://localhost:5000/rozarpay",{
 //     method:'POST'
 // }).then((t)=>t.json())
 // console.log(data)
 
+// axios.post('/your-webhook-endpoint', response)
+// .then((res) => {
+//   console.log('Notification sent successfully');
+// })
+// .catch((error) => {
+//   console.error('Error sending notification:', error);
+// });
 const options = {
 key:"rzp_test_BDAtyJxf1DyqOb",
-currency:"INR",
-amount:1000,
+currency:props.currency,
+amount:props.amt,
 // currency:data.currency,
 // amount:data.amount,
 description:'wallet Tranformation',
 // image:'https://localhost:5000/logo.jpg',
-order_id:112,
+// order_id:112,
 handle: function (response){
     alert("PAYMENT ID:"+ response.razorpay_payment_id)
     alert("ORDER ID:"+ response.razorpay_order_id)
 },
 prefill:{
-    name:"HINAKOUSER",
+    name:props.name,
     email:"hina123@gmail.com",
     contact:"*********"
 }
